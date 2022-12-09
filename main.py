c06902250cd69be249adde30cf9e3b4044e2584c
Davvos11 <vosdavid2@gmail.com>
2022-12-09 21:05:59 +0100

    add main pygame loop

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ def main():
     # Initialise pygame
     pygame.init()
 
+    # Show window
+    screen = pygame.display.set_mode((640, 240))
+
+    # Main loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
 
 if __name__ == '__main__':
     main()
